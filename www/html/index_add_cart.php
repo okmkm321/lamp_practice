5c2d78bda3950d18e5ed484f11c3e28117f25e93
okmkm321 <okmkm321@gmail.com>
2020-08-15 02:49:39 +0900

    #6 CSRF対策

--- www/html/index_add_cart.php
+++ www/html/index_add_cart.php
@@ -6,21 +6,31 @@ require_once MODEL_PATH . 'item.php';
 require_once MODEL_PATH . 'cart.php';
 
 session_start();
+header("X-FRAME-OPTIONS: DENY");
 
-if(is_logined() === false){
-  redirect_to(LOGIN_URL);
+if (is_valid_csrf_token($token) !== false) { 
+  if(is_logined() === false){
+    redirect_to(LOGIN_URL);
+  }
+
+  $db = get_db_connect();
+  $user = get_login_user($db);
+
+
+  $item_id = get_post('item_id');
+
+  if(add_cart($db,$user['user_id'], $item_id)){
+    set_message('カートに商品を追加しました。');
+  } else {
+    set_error('カートの更新に失敗しました。');
+  }
 }
 
-$db = get_db_connect();
-$user = get_login_user($db);
-
-
-$item_id = get_post('item_id');
-
-if(add_cart($db,$user['user_id'], $item_id)){
-  set_message('カートに商品を追加しました。');
-} else {
-  set_error('カートの更新に失敗しました。');
+if (isset($_POST["csrf_token"])) {
+  if ($_SESSION['csrf_token'] !== $_POST["csrf_token"]) {
+    echo '不正なアクセスです。';
+  } 
 }
+
 
 redirect_to(HOME_URL);
